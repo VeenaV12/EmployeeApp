48109afdddffe1255eb0d12921123a2bf53ea8da
Veena Vidyanandan <106391333+VeenaV12@users.noreply.github.com>
2024-07-06 22:08:41 +0530

    CORS ADDED

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,12 @@ const adminRoute = require('./Routes/adminRoute')
 const app = express()
 app.use(cors())
 
+app.use(cors({
+  origin:['https://employee-app-client.vercel.app'],
+  credentials:true,
+  methods:['GET','POST','PUT','DELETE']
+}))
+
 app.use('/user',userRoute)
 app.use('/admin',adminRoute)
 

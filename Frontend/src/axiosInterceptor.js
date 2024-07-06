d706d701a379b18562ca06fa239cee58ec5f9ded
Veena Vidyanandan <106391333+VeenaV12@users.noreply.github.com>
2024-07-06 21:55:06 +0530

    change in base url

--- Frontend/src/axiosInterceptor.js
+++ Frontend/src/axiosInterceptor.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL:'http://localhost:3000'
+    baseURL:'https://employee-app-server-delta.vercel.app'
 })
 
 axiosInstance.interceptors.request.use((config)=>{
